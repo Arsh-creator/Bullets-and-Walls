a4105fd17615eb48de4ff0be18339ffc8891fdca
Arsh-creator <75788096+Arsh-creator@users.noreply.github.com>
2021-05-18 20:23:42 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,44 +1,58 @@
-var bullet,wall,speed,weight,thikness;
-var bulletrightedge,bulletleftedge,wallleftedge;
+var bullet, wall, speed, weight, thickness,deformation, damage;
 function setup() {
-  createCanvas(1600,400);
-  speed=random(55,90);
-  weight=random(400,1500);
-  thickness=random(22,83);
-  bullet=createSprite(50,200,50,50);
-  wall=createSprite(1200,200,thickness,height/2);
-  wall.shapeColor="red";
-  
-
- bullet.velocityX=speed;
+  createCanvas(1600, 400);
+  thickness= random(22, 83);
+  speed= random(233, 321);
+  weight= random(30, 52);
+  bullet = createSprite(50, 200, 50, 10);
+  bullet.velocityX = speed;
+  bullet.shapeColor = color(255,255,255);
+  wall = createSprite(1200, 200, thickness, 100);
+  wall.shapeColor = "red";
 }
 
 function draw() {
-  background(255,255,255);
-if(hascollided(bullet, wall)){
+  background(0,0,0);  
+
+  if(wall.x-bullet.x<(bullet.width+wall.width)/2){
+   bullet.velocityX = 0;
+   deformation= 0.5*weight*speed*speed/22509;
+  }
+
+  if(deformation>180){
+    bullet.shapeColor = "white";
+  }
+
+  if(deformation<180 && deformation>100){
+    bullet.shapeColor = "white";
+  }
+
+  if(deformation<100){
+    bullet.shapeColor = "white";
+  }
+
+  if(hasCollided(bullet, wall)){
   bullet.velocityX=0;
-  var damage=0.5 * weight * speed * speed/(thickness+thickness+thickness);
+  damage= 0.5*weight*speed*speed/(thickness*thickness*thickness);  
+  }
 
   if(damage>10){
-    wall.shapeColor="red";
-
-  }
-  if(damage<10){
-    wall.shapeColor="green";
-  }
-}
-
+    bullet.shapeColor = color(255, 0, 0);
+    }
   
-  
+    if(damage<10){
+      bullet.shapeColor = color(0, 255, 0);
+    } 
+   
+
   drawSprites();
 }
 
-function hascollided (lbullet,lwall){
-bulletrightedge=lbullet.x+lbullet.width;
-wallleftedge=lwall.x;
-if(bulletleftedge>=wallleftedge){
-  return true
-
-}
-return false
+function hasCollided(lbullet, lwall){
+  bulletrightedge = lbullet.x + lbullet.width;
+  wallleftedge= lwall.x;
+  if(bulletrightedge>=wallleftedge){
+    return true
+  }
+  return false;
 }
